96148dd84c7b317ddcca81461c05ec8b26e41d29
n163lph03n1x <myke_wills@hotmail.com>
2021-02-07 22:43:33 -0400

    Initial Commit

---
+++ BrickGame/Assets/OptimizeSystem.cs
@@ -0,0 +1,87 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using Rewired;
+using System;
+
+public class OptimizeSystem : MonoBehaviour
+{
+    public static BinaryFormatter _BinaryFormatter = new BinaryFormatter();
+    public static Color _colorAlpha = new Color();
+    public static Player _InputPlayer;
+    public static bool _Active = false;
+    public static Vector3 _Vector3 = new Vector3();
+    public static Vector2 _Vector2 = new Vector2();
+    public static Quaternion _Quaternion = new Quaternion();
+    public static IEnumerator routine = null;
+    public static Func<bool> func;
+    public static WaitUntil _waitUntil = new WaitUntil(func);
+    static Dictionary<float, WaitForSeconds> _timeInterval = new Dictionary<float, WaitForSeconds>(100);
+    static Dictionary<float, WaitForSecondsRealtime> _realTimeInterval = new Dictionary<float, WaitForSecondsRealtime>(100);
+    public static WaitForEndOfFrame _endOfFrame = new WaitForEndOfFrame();
+    public static WaitForFixedUpdate _fixedUpdate = new WaitForFixedUpdate();
+    public void Awake() { _InputPlayer = ReInput.players.GetPlayer(0); }
+    public static Player playerController { get { return _InputPlayer; } }
+    public static WaitForEndOfFrame EndOfFrame { get { return _endOfFrame; } }
+    public static WaitUntil WaitFor(bool active) { func = () => active; return _waitUntil; }
+    public static WaitForFixedUpdate FixedUpdate { get { return _fixedUpdate; } }
+    public static WaitForSeconds Wait(float seconds)
+    {
+        if (!_timeInterval.ContainsKey(seconds))
+            _timeInterval.Add(seconds, new WaitForSeconds(seconds));
+        return _timeInterval[seconds];
+    }
+    public static WaitForSecondsRealtime WaitRealtime(float seconds)
+    {
+        if (!_realTimeInterval.ContainsKey(seconds))
+            _realTimeInterval.Add(seconds, new WaitForSecondsRealtime(seconds));
+        return _realTimeInterval[seconds];
+    }
+    public static BinaryFormatter ChangeBinaryFormatter
+    {
+        get { return _BinaryFormatter; }
+    }
+    public static Color ChangeColor(float r, float g, float b, float a)
+    {
+        _colorAlpha.r = r;
+        _colorAlpha.g = g;
+        _colorAlpha.b = b;
+        _colorAlpha.a = a;
+        return _colorAlpha;
+    }
+
+    public static Vector3 ChangeVector3(float x, float y, float z)
+    {
+        _Vector3.x = x;
+        _Vector3.y = y;
+        _Vector3.z = z;
+        return _Vector3;
+    }
+    public static Vector2 ChangeVector2(float x, float y)
+    {
+        _Vector2.x = x;
+        _Vector2.y = y;
+        return _Vector2;
+    }
+    public static Quaternion ChangeQuaternion(float x, float y, float z, float w)
+    {
+        _Quaternion.x = x;
+        _Quaternion.y = y;
+        _Quaternion.y = z;
+        _Quaternion.y = w;
+        return _Quaternion;
+    }
+    public static void ClearOutRenderTexture(RenderTexture renderTexture)
+    {
+        RenderTexture rt = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+        GL.Clear(true, true, Color.clear);
+        RenderTexture.active = rt;
+    }
+    public static void GameMouseActive(bool active, CursorLockMode lockMode)
+    {
+        Cursor.lockState = lockMode;
+        Cursor.visible = active;
+    }
+}
---
+++ BrickGame/Assets/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static bool isStarted = false;
+    public float acceleration;
+    public float moveSpeed = 5;
+    public bool isBoosting = true;
+    public Transform ballPool;
+    float maxX = 13.3f;
+    float minX = -13.3f;    
+    float maxY = -3.5f;
+    float minY = -4.5f;
+    Vector3 returnPos = new Vector3(0, -4, 0);
+    Vector2 velocity;
+    private void Start()
+    {
+        OptimizeSystem.GameMouseActive(false, CursorLockMode.Locked);
+        SetupPaddle();
+    }
+
+    void Update()
+    {
+        Move();
+        if (OptimizeSystem.playerController.GetButton("Boost")) isBoosting = true;
+        else if (OptimizeSystem.playerController.GetButtonUp("Boost")) isBoosting = false;
+        if (OptimizeSystem.playerController.GetButtonDown("Start") && !isStarted) StartPaddle();
+        if (OptimizeSystem.playerController.GetButtonDown("A") && isStarted) SetupPaddle();
+
+    }
+    void Move()
+    {
+        float inputX = OptimizeSystem.playerController.GetAxisRaw("Horizontal");
+        float inputY = OptimizeSystem.playerController.GetAxisRaw("Vertical");
+        transform.Translate(Time.deltaTime * velocity);
+        if(inputX != 0)
+        {
+            if (isBoosting)
+                velocity.x = Mathf.MoveTowards(velocity.x, (moveSpeed * 1.5f) * inputX, acceleration);
+            else
+                velocity.x = Mathf.MoveTowards(velocity.x, moveSpeed * inputX, acceleration);
+        }
+        else if(inputX == 0)
+            velocity.x = 0;
+
+        if (inputY != 0)
+        {
+            if (isBoosting)
+                velocity.y = Mathf.MoveTowards(velocity.y, (moveSpeed * 1.5f) * inputY, acceleration);
+            else
+                velocity.y = Mathf.MoveTowards(velocity.y, moveSpeed * inputY, acceleration);
+        }
+        else if (inputY == 0)
+            velocity.y = 0;
+
+        if (transform.localPosition.y < minY)
+            transform.localPosition = OptimizeSystem.ChangeVector3(transform.localPosition.x, minY, transform.localPosition.z);
+        else if (transform.localPosition.y > maxY)
+            transform.localPosition = OptimizeSystem.ChangeVector3(transform.localPosition.x, maxY, transform.localPosition.z);
+        if (transform.localPosition.x < minX)
+            transform.localPosition = OptimizeSystem.ChangeVector3(minX, transform.localPosition.y, transform.localPosition.z);
+        else if (transform.localPosition.x > maxX)
+            transform.localPosition = OptimizeSystem.ChangeVector3(maxX, transform.localPosition.y, transform.localPosition.z);
+
+
+
+
+
+    }
+    public void StartPaddle()
+    {
+        transform.GetChild(1).transform.SetParent(ballPool);
+        isStarted = true;
+        
+    }
+    public void SetupPaddle()
+    {
+        isStarted = false;
+        transform.localPosition = returnPos;
+        if (ballPool.transform.childCount > 0)
+        {
+            ballPool.transform.GetChild(0).transform.SetParent(transform);
+            transform.GetChild(1).transform.localPosition = OptimizeSystem.ChangeVector3(0, 0.5f, 0);
+            transform.GetChild(1).transform.localRotation = Quaternion.identity;
+        }
+    }
+
+}
